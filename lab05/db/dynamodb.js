140c7ab33cf461f05cc8bf1ee7b791a1a3d98679
NHKizChilling <nhoangkhai195@gmail.com>
2026-03-08 20:36:50 +0700

    Update for testing Elastic Beanstalk

--- lab05/db/dynamodb.js
+++ lab05/db/dynamodb.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
 const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 
-const client = new DynamoDBClient({
-    region: process.env.AWS_REGION,
-    endpoint: process.env.DYNAMODB_ENDPOINT,
-    credentials: {
+const config = {
+    region: process.env.AWS_REGION || 'ap-southeast-1'
+};
+
+if (process.env.DYNAMODB_ENDPOINT) {
+    config.endpoint = process.env.DYNAMODB_ENDPOINT;
+}
+
+if (process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY) {
+    config.credentials = {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
-});
+    };
+}
+
+const client = new DynamoDBClient(config);
 
 const dynamoDb = DynamoDBDocumentClient.from(client, {
     marshallOptions: {
